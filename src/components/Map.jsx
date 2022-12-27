69f75881ab8837acf99de7f87a9db89a724be94a
Apriyadi Hegarto <ahegarto@gmail.com>
2022-12-27 23:31:22 +0700

    tambah peta

---
+++ src/components/Map.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { MapContainer,TileLayer, Marker, Popup} from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const center = [-6.17523297070254, 106.82718123499757];
+
+const position =  [-6.171851653295678, 106.82595814774415]
+
+export default function App() {
+  return (
+    <MapContainer
+      center={center}
+      zoom={15}
+      style={{ width: '400px', height: '400px' }}
+    >
+      <TileLayer
+        url="https://api.maptiler.com/maps/basic-v2/{z}/{x}/{y}.png?key=caADrFh1FFN8pQYpkQWk"
+        attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+      />
+      <Marker position={position}>
+        <Popup>
+          Tulisan di dalam pop up.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Test from "../pages/Test";
+import Map from "../pages/map";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Test />} />
+        <Route path="/map" element={<Map />} />
       </Routes>
     </BrowserRouter>
   );
